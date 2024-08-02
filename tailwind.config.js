797d811d8a5265a504ccd385c331f4c686c7dc23
Chinenye <badnenye@gmail.com>
2024-08-02 19:45:56 +0100

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          100: "hsl(322, 100%, 66%)",
+          200: "hsl(193, 100%, 96%)",
+          300: "hsl(192, 100%, 9%)",
+          400: "hsl(208, 11%, 55%)",
+        },
+      },
+      fontFamily: {
+        heading: ["Poppins"],
+        body: ["Open Sans"],
+      },
+      backgroundImage: URL(),
+    },
   },
   plugins: [],
 };
